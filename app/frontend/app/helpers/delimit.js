1105206570de29d96602503506108e2098134830
Brian Whitmer <brian.whitmer@gmail.com>
2017-04-08 15:58:21 -0600

    full dollar amount

--- app/frontend/app/helpers/delimit.js
+++ app/frontend/app/helpers/delimit.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Helper.helper(function(params) {
-  return Ember.templateHelpers.delimit(params[0]);
+  return Ember.templateHelpers.delimit(params[0], params[1]);
 });
