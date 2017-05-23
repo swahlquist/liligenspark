51f2a92901e80c9622e8e6a552763918fe61d402
Brian Whitmer <brian.whitmer@gmail.com>
2017-05-23 00:10:33 -0600

    don't show integration frame until we know it's actually there and loaded

--- app/frontend/app/components/embed-frame.js
+++ app/frontend/app/components/embed-frame.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 import frame_listener from '../utils/frame_listener';
+import i18n from '../utils/i18n';
+import persistence from '../utils/persistence';
 
 export default Ember.Component.extend({
   tagName: 'div',
@@ -28,6 +30,20 @@ export default Ember.Component.extend({
 //         });
       }
     });
+    Ember.run.later(function() {
+      if(Ember.$(elem).find("#integration_overlay.pending").length > 0) {
+        Ember.$(elem).find("#integration_overlay .status").text(i18n.t('loading_integration', "Loading Integration..."));
+      }
+    }, 750);
+    Ember.run.later(function() {
+      if(Ember.$(elem).find("#integration_overlay.pending").length > 0) {
+        if(!persistence.get('online')) {
+          Ember.$(elem).find("#integration_overlay .status").text(i18n.t('loading_integration_failed_offline', "Integrations cannot load when offline"));
+        } else {
+          Ember.$(elem).find("#integration_overlay .status").text(i18n.t('loading_integration_failed', "Integration hasn't loaded, please check your settings and Internet Connection"));
+        }
+      }
+    }, 5000);
   },
   overlay_style: function() {
     var res = this.get('board_style');
