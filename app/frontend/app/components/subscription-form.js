361f1d6a28aba673d6acdaeb38c6c40f169bd5d6
Brian Whitmer <brian.whitmer@gmail.com>
2017-09-18 17:11:48 -0600

    logout option on blocking subscription modal

--- app/frontend/app/components/subscription-form.js
+++ app/frontend/app/components/subscription-form.js
@@ -56,6 +56,9 @@ export default Ember.Component.extend({
     reload: function() {
       location.reload();
     },
+    logout: function() {
+      session.invalidate(true);
+    },
     reset: function() {
       this.get('subscription').reset();
     },
