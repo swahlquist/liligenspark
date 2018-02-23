d2546a191bd12dbb36b9b46b1a858ab93deb9cd2
Brian Whitmer <brian.whitmer@gmail.com>
2018-02-23 11:20:36 -0700

    squelch guard errors for now

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,17 @@
+if ENV['RAILS_ENV'] == 'development'
+  # TODO: this is a dumb workaround for not understanding how Guard works
+  require 'listen/record/symlink_detector'
+  module Listen
+    class Record
+      class SymlinkDetector
+        def _fail(_, _)
+          fail Error, "Don't watch locally-symlinked directory twice"
+        end
+      end
+    end
+  end
+end
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 require 'go_secure'
