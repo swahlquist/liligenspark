bacac4465a8a0f6d1e289116d5e67955715fe9a6
Brian Whitmer <brian.whitmer@gmail.com>
2018-03-29 14:53:46 -0600

    deployment compilation for 2018.03.29

--- lib/feature_flags.rb
+++ lib/feature_flags.rb
@@ -10,7 +10,7 @@ module FeatureFlags
   ENABLED_FRONTEND_FEATURES = ['subscriptions', 'assessments', 'custom_sidebar', 'snapshots',
               'video_recording', 'goals', 'modeling', 'geo_sidebar', 'edit_before_copying',
               'core_reports', 'lessonpix', 'translation', 'fast_render',
-              'audio_recordings', 'app_connections']
+              'audio_recordings', 'app_connections', 'enable_all_buttons']
   DISABLED_CANARY_FEATURES = []
   FEATURE_DATES = {
     'word_suggestion_images' => 'Jan 21, 2017',
