8e05b21113dac170f76e7e8d06308eaa8e4bbe7d
Brian Whitmer <brian.whitmer@gmail.com>
2018-08-09 16:55:19 -0600

    show a message when selecting a layout

--- app/frontend/app/controllers/home-boards.js
+++ app/frontend/app/controllers/home-boards.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import modal from '../utils/modal';
+import i18n from '../utils/i18n';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
@@ -8,6 +10,9 @@ export default Controller.extend({
     },
     select_board: function() {
       this.transitionToRoute('index');
+      runLater(function() {
+        modal.success(i18n.t('board_layout_copying', "Great, we've got just the board for you! CoughDrop is making your own personal copy based on your choices. You can start using it by entering Speak Mode when you're ready."), true, true);
+      }, 100);
     }
   }
 });
