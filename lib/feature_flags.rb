767dfb8d4137b9cedab6cfdc6406bfcae79a98c5
Brian Whitmer <brian.whitmer@gmail.com>
2018-09-11 15:54:45 -0600

    enable premium symbols

--- lib/feature_flags.rb
+++ lib/feature_flags.rb
@@ -7,7 +7,8 @@ module FeatureFlags
   ENABLED_FRONTEND_FEATURES = ['subscriptions', 'assessments', 'custom_sidebar', 'snapshots',
               'video_recording', 'goals', 'modeling', 'geo_sidebar', 'edit_before_copying',
               'core_reports', 'lessonpix', 'translation', 'fast_render',
-              'audio_recordings', 'app_connections', 'enable_all_buttons', 'badge_progress']
+              'audio_recordings', 'app_connections', 'enable_all_buttons', 'badge_progress',
+              'premium_symbols']
   DISABLED_CANARY_FEATURES = []
   FEATURE_DATES = {
     'word_suggestion_images' => 'Jan 21, 2017',
