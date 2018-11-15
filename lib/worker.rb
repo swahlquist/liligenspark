8a542e2e3e855ae313938e73e10580f31c9390e9
Brian Whitmer <brian.whitmer@gmail.com>
2018-11-14 23:05:51 -0500

    helper for failed queues

--- lib/worker.rb
+++ lib/worker.rb
@@ -12,5 +12,15 @@ module Worker
     puts JSON.pretty_generate(methods.to_a.sort_by(&:last))
     methods
   end
+
+  def self.requeue_failed(method)
+    count = Resque::Failure.count
+    count.times do |i|
+      Resque::Failure.requeue(i)
+    end
+    count.times do |i|
+      Resque::Failure.remove(0)
+    end
+  end
 end
 
