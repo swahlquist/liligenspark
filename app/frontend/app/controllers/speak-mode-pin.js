8829794fb56a9b8ea4415d8a16d17196ab3edd20
Brian Whitmer <brian.whitmer@gmail.com>
2018-11-14 21:38:01 -0500

    fix speak mode pin to not prompt for password remembering

--- app/frontend/app/controllers/speak-mode-pin.js
+++ app/frontend/app/controllers/speak-mode-pin.js
@@ -22,6 +22,29 @@ export default modal.ModalController.extend({
   opening: function() {
     this.set('pin', '');
   },
+  update_pin: function() {
+    var str = this.get('pin_dots') || "";
+    var pin = this.get('pin');
+    for(var idx = 0; idx < str.length; idx++) {
+      if(str[idx] != "●") {
+        pin = pin + str[idx];
+      }
+    }
+    if(pin != this.get('pin')) {
+      this.set('pin', pin);
+    }
+  }.observes('pin_dots'),
+  update_pin_dots: function() {
+    var str = "●";
+    var res = "";
+    var steps = (this.get('pin') || '').length;
+    for(var idx = 0; idx < steps; idx++) {
+      res = res + str;
+    }
+    if(res != this.get('pin_dots')) {
+      this.set('pin_dots', res);
+    }
+  }.observes('pin'),
   actions: {
     add_digit: function(digit) {
       var pin = this.get('pin') || "";
