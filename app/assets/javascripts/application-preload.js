6766be95afce1e8fe8959866d74678f8019f2efc
Brian Whitmer <brian.whitmer@gmail.com>
2018-11-23 15:10:55 -0700

    deployment compilation for 2018.11.23

--- app/assets/javascripts/application-preload.js
+++ app/assets/javascripts/application-preload.js
@@ -1,4 +1,8 @@
 //= require simple_state.js
 //= require globals.js
 
+<<<<<<< HEAD
 window.app_version = "2018.11.27";
+=======
+window.app_version = "2018.11.23";
+>>>>>>> 67d5df87... deployment compilation for 2018.11.23
