5c41ff5f1fc4250127357f6383298557e4bc51b6
Brian Whitmer <brian.whitmer@gmail.com>
2018-11-29 15:11:08 -0700

    deployment compilation for 2018.11.29

--- app/assets/javascripts/application-preload.js
+++ app/assets/javascripts/application-preload.js
@@ -2,7 +2,11 @@
 //= require globals.js
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 window.app_version = "2018.11.27";
 =======
 window.app_version = "2018.11.23";
 >>>>>>> 67d5df87... deployment compilation for 2018.11.23
+=======
+window.app_version = "2018.11.29";
+>>>>>>> 0eb61379... deployment compilation for 2018.11.29
