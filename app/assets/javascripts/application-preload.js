d58e3bbc8f4a192ec38085dec0df13570e046d02
Brian Whitmer <brian.whitmer@gmail.com>
2018-11-30 14:32:33 -0700

    deployment compilation for 2018.11.30a

--- app/assets/javascripts/application-preload.js
+++ app/assets/javascripts/application-preload.js
@@ -4,6 +4,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 window.app_version = "2018.11.27";
 =======
 window.app_version = "2018.11.23";
@@ -14,3 +15,6 @@ window.app_version = "2018.11.29";
 =======
 window.app_version = "2018.11.30";
 >>>>>>> 75c3b643... deployment compilation for 2018.11.30
+=======
+window.app_version = "2018.11.30a";
+>>>>>>> 2681f908... deployment compilation for 2018.11.30a
