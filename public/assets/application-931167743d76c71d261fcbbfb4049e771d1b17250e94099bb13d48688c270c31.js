f6a65d4e67974b902e623f7c35384bb171ddd4c9
Brian Whitmer <brian.whitmer@gmail.com>
2019-01-07 15:28:37 -0700

    deployment compilation for 2019.01.07

--- public/assets/application-931167743d76c71d261fcbbfb4049e771d1b17250e94099bb13d48688c270c31.js
+++ public/assets/application-931167743d76c71d261fcbbfb4049e771d1b17250e94099bb13d48688c270c31.js
@@ -13,7 +13,7 @@ window.user_preferences = {"device":{"voice":{"pitch":1.0,"volume":1.0},"button_
 
 
 
-window.app_version = "2019.01.04d";
+window.app_version = "2019.01.07";
 window.EmberENV={FEATURES:{}}
 var loader,define,requireModule,require,requirejs,runningTests=!1
 function createDeprecatedModule(e){define(e,["exports","ember-resolver/resolver","ember"],function(t,n,r){r.default.deprecate("Usage of `"+e+"` module is deprecated, please update to `ember-resolver`.",!1,{id:"ember-resolver.legacy-shims",until:"3.0.0"}),t.default=n.default})}if(function(e){"use strict"
@@ -7999,7 +7999,7 @@ r.get("results")||r.set("loading",!0),r.set("error",null)
 this.get("model.include_other_boards")
 if(e.get("button_set")){var l=o.default.get("currentUser"),i=o.default.get("speak_mode"),d=(new Date).getTime(),u=Math.random()+"-"+d
 r.set("search_id",u)
-var c="iOS"==s.default.system?100:null
+var c="iOS"==s.default.system?200:null
 Ember.run.later(function(){r.get("search_id")==u&&(o.default.get("feature_flags.find_multiple_buttons")?e.get("button_set").find_sequence(r.get("searchString"),e.get("id"),l,i):e.get("button_set").find_buttons(r.get("searchString"),e.get("id"),l,i)).then(function(e){if(console.log("results!",e,(new Date).getTime()-d),n.default.get("online"))r.set("results",e),r.set("loading",!1)
 else{var t=[]
 e.forEach(function(e){var n=[e.image]
@@ -10878,8 +10878,8 @@ var d,u=[],c=[]
 for(a=0;a<i;++a)u[a]=a,c[a]=t.charCodeAt(a)
 for(u[i]=i,a=0;a<l;++a){for(s=a+1,o=0;o<i;++o)n=s,d=e.charCodeAt(a)===c[o],(s=u[o]+(d?0:1))>(r=n+1)&&(s=r),s>(r=u[o+1]+1)&&(s=r),u[o]=n
 u[o]=s}return s}}).create({pieces:10,max_results:5})
-e.default=r}),define("frontend/config/environment",[],function(){var e={default:{modulePrefix:"frontend",environment:"production",rootURL:"/",locationType:"auto",EmberENV:{FEATURES:{}},APP:{name:"frontend",version:"0.0.2+2a46df1c"},exportApplicationGlobal:!1}}
-return Object.defineProperty(e,"__esModule",{value:!0}),e}),runningTests||require("frontend/app").default.create({name:"frontend",version:"0.0.2+2a46df1c"})
+e.default=r}),define("frontend/config/environment",[],function(){var e={default:{modulePrefix:"frontend",environment:"production",rootURL:"/",locationType:"auto",EmberENV:{FEATURES:{}},APP:{name:"frontend",version:"0.0.2+986318d0"},exportApplicationGlobal:!1}}
+return Object.defineProperty(e,"__esModule",{value:!0}),e}),runningTests||require("frontend/app").default.create({name:"frontend",version:"0.0.2+986318d0"})
 ;
 
 
