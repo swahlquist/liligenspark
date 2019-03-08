18bc48e2e10a51ae9c61093de6c5269e9b6adc02
Brian Whitmer <brian.whitmer@gmail.com>
2019-03-08 11:48:48 -0700

    re-enable legacy links for now

--- app/models/user_link.rb
+++ app/models/user_link.rb
@@ -79,212 +79,212 @@ class UserLink < ApplicationRecord
         'state' => link.data['state']
       }
     end
-    # Stop supporting legacy links, maybe? ...
-    # if record.is_a?(User) && record.settings
-    #   # include old-school supervisors, supervisees, org connections, board shares
-    #   (record.settings['boards_shared_with_me'] || []).each do |share|
-    #     res << {
-    #       'user_id' => record.global_id,
-    #       'record_code' => "Board:#{share['board_id']}",
-    #       'type' => 'board_share',
-    #       'old_school' => true,
-    #       'state' => {
-    #         'include_downstream' => !!share['include_downstream'],
-    #         'allow_editing' => !!share['allow_editing'],
-    #         'pending' => !!share['pending'],
-    #         'board_key' => share['board_key'],
-    #         'user_name' => record.user_name
-    #       }
-    #     }
-    #   end
+    # TODO: Stop supporting legacy links, maybe? ...
+    if record.is_a?(User) && record.settings
+      # include old-school supervisors, supervisees, org connections, board shares
+      (record.settings['boards_shared_with_me'] || []).each do |share|
+        res << {
+          'user_id' => record.global_id,
+          'record_code' => "Board:#{share['board_id']}",
+          'type' => 'board_share',
+          'old_school' => true,
+          'state' => {
+            'include_downstream' => !!share['include_downstream'],
+            'allow_editing' => !!share['allow_editing'],
+            'pending' => !!share['pending'],
+            'board_key' => share['board_key'],
+            'user_name' => record.user_name
+          }
+        }
+      end
 
-    #   (record.settings['supervisors'] || []).each do |sup|
-    #     res << {
-    #       'user_id' => record.global_id,
-    #       'record_code' => "User:#{sup['user_id']}",
-    #       'type' => 'supervisor',
-    #       'old_school' => true,
-    #       'state' => {
-    #         'edit_permission' => sup['edit_permission'],
-    #         'organization_unit_ids' => sup['organization_unit_ids']
-    #       }
-    #     }
-    #   end
-    #   (record.settings['supervisees'] || []).each do |sup|
-    #     res << {
-    #       'user_id' => sup['user_id'],
-    #       'record_code' => Webhook.get_record_code(record),
-    #       'type' => 'supervisor',
-    #       'old_school' => true,
-    #       'state' => {
-    #         'edit_permission' => sup['edit_permission'],
-    #         'organization_unit_ids' => sup['organization_unit_ids']
-    #       }
-    #     }
-    #   end
-    #   org_ids = []
-    #   (record.settings['managed_by'] || {}).each do |org_id, opts|
-    #     org_ids << org_id
-    #     res << {
-    #       'user_id' => record.global_id,
-    #       'record_code' => "Organization:#{org_id}",
-    #       'type' => 'org_user',
-    #       'old_school' => true,
-    #       'state' => {
-    #         'sponsored' => opts['sponsored'],
-    #         'pending' => opts['pending']
-    #       }
-    #     }
-    #   end
-    #   (record.settings['manager_for'] || {}).each do |org_id, opts|
-    #     org_ids << org_id
-    #     res << {
-    #       'user_id' => record.global_id,
-    #       'record_code' => "Organization:#{org_id}",
-    #       'type' => 'org_manager',
-    #       'old_school' => true,
-    #       'state' => {
-    #         'full_manager' => opts['full_manager']
-    #       }
-    #     }
-    #   end
-    #   (record.settings['supervisor_for'] || {}).each do |org_id, opts|
-    #     org_ids << org_id
-    #     res << {
-    #       'user_id' => record.global_id,
-    #       'record_code' => "Organization:#{org_id}",
-    #       'type' => 'org_supervisor',
-    #       'old_school' => true,
-    #       'state' => {
-    #         'pending' => opts['pending']
-    #       }
-    #     }
-    #   end
-    #   if org_ids.length > 0
-    #     possible_units = OrganizationUnit.where(:organization_id => record.class.local_ids(org_ids))
-    #     possible_units.each do |unit|
-    #       sup = (unit.settings['supervisors'] || []).detect{|s| s['user_id'] == record.global_id }
-    #       comm = (unit.settings['communicators'] || []).detect{|c| c['user_id'] == record.global_id }
-    #       if sup
-    #         res << {
-    #           'user_id' => record.global_id,
-    #           'record_code' => Webhook.get_record_code(unit),
-    #           'type' => 'org_unit_supervisor',
-    #           'old_school' => true,
-    #           'state' => {
-    #             'edit_permission' => sup['edit_permission']
-    #           }
-    #         }
-    #       end
-    #       if comm
-    #         res << {
-    #           'user_id' => record.global_id,
-    #           'record_code' => Webhook.get_record_code(unit),
-    #           'type' => 'org_unit_communicator',
-    #           'old_school' => true,
-    #           'state' => {}
-    #         }
-    #       end
-    #     end
-    #   end
-    # elsif record.is_a?(Organization)
-    #   # include org connections
-    #   record.settings ||= {}
-    #   record_code = Webhook.get_record_code(record)
-    #   (record.settings['attached_user_ids'] || {}).each do |type, ids|
-    #     ids.each do |user_id|
-    #       if type == 'user'
-    #         sponsored = !!((record.settings['attached_user_ids'] || {})['sponsored_user'] || []).detect{|id| id == user_id }
-    #         approved = !!((record.settings['attached_user_ids'] || {})['approved_user'] || []).detect{|id| id == user_id }
-    #         res << {
-    #           'user_id' => user_id,
-    #           'record_code' => record_code,
-    #           'type' => 'org_user',
-    #           'old_school' => true,
-    #           'state' => {
-    #             'pending' => !approved,
-    #             'sponsored' => sponsored,
-    #             'eval' => false
-    #           }
-    #         }
-    #       elsif type == 'manager'
-    #         res << {
-    #           'user_id' => user_id,
-    #           'record_code' => record_code,
-    #           'type' => 'org_manager',
-    #           'old_school' => true,
-    #           'state' => {
-    #             'full_manager' => true # TODO: this isn't always true...
-    #           }
-    #         }
-    #       elsif type == 'supervisor'
-    #         res << {
-    #           'user_id' => user_id,
-    #           'record_code' => record_code,
-    #           'type' => 'org_supervisor',
-    #           'old_school' => true,
-    #           'state' => {}
-    #         }
-    #       elsif type == 'subscription'
-    #         res << {
-    #           'user_id' => user_id,
-    #           'record_code' => record_code,
-    #           'type' => 'org_subscription',
-    #           'old_school' => true,
-    #           'state' => {}
-    #         }
-    #       end
-    #     end
-    #   end
-    # elsif record.is_a?(OrganizationUnit)
-    #   # include org unit connections
-    #   (record.settings['supervisors'] || []).each do |sup|
-    #     res << {
-    #       'user_id' => sup['user_id'],
-    #       'record_code' => record_code,
-    #       'type' => 'org_unit_supervisor',
-    #       'old_school' => true,
-    #       'state' => {
-    #         'user_name' => sup['user_name'],
-    #         'edit_permission' => sup['edit_permission']
-    #       }
-    #     }
-    #   end
-    #   (record.settings['communicators'] || []).each do |com|
-    #     res << {
-    #       'user_id' => com['user_id'],
-    #       'record_code' => record_code,
-    #       'type' => 'org_unit_communicator',
-    #       'old_school' => true,
-    #       'state' => {
-    #         'user_name' => com['user_name']
-    #       }
-    #     }
-    #   end
-    # elsif record.is_a?(Board)
-    #   # include board shares
-    #   author = record.user
-    #   if author
-    #     shares = (author.settings['boards_i_shared'] || {})[record.global_id]
-    #     (shares || []).each do |share|
-    #       res << {
-    #         'user_id' => share['user_id'],
-    #         'record_code' => record_code,
-    #         'type' => 'board_share',
-    #         'old_school' => true,
-    #         'state' => {
-    #           'board_key' => record.key,
-    #           'sharer_id' => author.global_id,
-    #           'sharer_user_name' => author.user_name,
-    #           'include_downstream' => !!share['include_downstream'],
-    #           'allow_editing' => !!share['allow_editing'],
-    #           'pending' => !!share['pending'],
-    #           'user_name' => share['user_name']
-    #         }
-    #       }
-    #     end
-    #   end
-    # end
+      (record.settings['supervisors'] || []).each do |sup|
+        res << {
+          'user_id' => record.global_id,
+          'record_code' => "User:#{sup['user_id']}",
+          'type' => 'supervisor',
+          'old_school' => true,
+          'state' => {
+            'edit_permission' => sup['edit_permission'],
+            'organization_unit_ids' => sup['organization_unit_ids']
+          }
+        }
+      end
+      (record.settings['supervisees'] || []).each do |sup|
+        res << {
+          'user_id' => sup['user_id'],
+          'record_code' => Webhook.get_record_code(record),
+          'type' => 'supervisor',
+          'old_school' => true,
+          'state' => {
+            'edit_permission' => sup['edit_permission'],
+            'organization_unit_ids' => sup['organization_unit_ids']
+          }
+        }
+      end
+      org_ids = []
+      (record.settings['managed_by'] || {}).each do |org_id, opts|
+        org_ids << org_id
+        res << {
+          'user_id' => record.global_id,
+          'record_code' => "Organization:#{org_id}",
+          'type' => 'org_user',
+          'old_school' => true,
+          'state' => {
+            'sponsored' => opts['sponsored'],
+            'pending' => opts['pending']
+          }
+        }
+      end
+      (record.settings['manager_for'] || {}).each do |org_id, opts|
+        org_ids << org_id
+        res << {
+          'user_id' => record.global_id,
+          'record_code' => "Organization:#{org_id}",
+          'type' => 'org_manager',
+          'old_school' => true,
+          'state' => {
+            'full_manager' => opts['full_manager']
+          }
+        }
+      end
+      (record.settings['supervisor_for'] || {}).each do |org_id, opts|
+        org_ids << org_id
+        res << {
+          'user_id' => record.global_id,
+          'record_code' => "Organization:#{org_id}",
+          'type' => 'org_supervisor',
+          'old_school' => true,
+          'state' => {
+            'pending' => opts['pending']
+          }
+        }
+      end
+      if org_ids.length > 0
+        possible_units = OrganizationUnit.where(:organization_id => record.class.local_ids(org_ids))
+        possible_units.each do |unit|
+          sup = (unit.settings['supervisors'] || []).detect{|s| s['user_id'] == record.global_id }
+          comm = (unit.settings['communicators'] || []).detect{|c| c['user_id'] == record.global_id }
+          if sup
+            res << {
+              'user_id' => record.global_id,
+              'record_code' => Webhook.get_record_code(unit),
+              'type' => 'org_unit_supervisor',
+              'old_school' => true,
+              'state' => {
+                'edit_permission' => sup['edit_permission']
+              }
+            }
+          end
+          if comm
+            res << {
+              'user_id' => record.global_id,
+              'record_code' => Webhook.get_record_code(unit),
+              'type' => 'org_unit_communicator',
+              'old_school' => true,
+              'state' => {}
+            }
+          end
+        end
+      end
+    elsif record.is_a?(Organization)
+      # include org connections
+      record.settings ||= {}
+      record_code = Webhook.get_record_code(record)
+      (record.settings['attached_user_ids'] || {}).each do |type, ids|
+        ids.each do |user_id|
+          if type == 'user'
+            sponsored = !!((record.settings['attached_user_ids'] || {})['sponsored_user'] || []).detect{|id| id == user_id }
+            approved = !!((record.settings['attached_user_ids'] || {})['approved_user'] || []).detect{|id| id == user_id }
+            res << {
+              'user_id' => user_id,
+              'record_code' => record_code,
+              'type' => 'org_user',
+              'old_school' => true,
+              'state' => {
+                'pending' => !approved,
+                'sponsored' => sponsored,
+                'eval' => false
+              }
+            }
+          elsif type == 'manager'
+            res << {
+              'user_id' => user_id,
+              'record_code' => record_code,
+              'type' => 'org_manager',
+              'old_school' => true,
+              'state' => {
+                'full_manager' => true # TODO: this isn't always true...
+              }
+            }
+          elsif type == 'supervisor'
+            res << {
+              'user_id' => user_id,
+              'record_code' => record_code,
+              'type' => 'org_supervisor',
+              'old_school' => true,
+              'state' => {}
+            }
+          elsif type == 'subscription'
+            res << {
+              'user_id' => user_id,
+              'record_code' => record_code,
+              'type' => 'org_subscription',
+              'old_school' => true,
+              'state' => {}
+            }
+          end
+        end
+      end
+    elsif record.is_a?(OrganizationUnit)
+      # include org unit connections
+      (record.settings['supervisors'] || []).each do |sup|
+        res << {
+          'user_id' => sup['user_id'],
+          'record_code' => record_code,
+          'type' => 'org_unit_supervisor',
+          'old_school' => true,
+          'state' => {
+            'user_name' => sup['user_name'],
+            'edit_permission' => sup['edit_permission']
+          }
+        }
+      end
+      (record.settings['communicators'] || []).each do |com|
+        res << {
+          'user_id' => com['user_id'],
+          'record_code' => record_code,
+          'type' => 'org_unit_communicator',
+          'old_school' => true,
+          'state' => {
+            'user_name' => com['user_name']
+          }
+        }
+      end
+    elsif record.is_a?(Board)
+      # include board shares
+      author = record.user
+      if author
+        shares = (author.settings['boards_i_shared'] || {})[record.global_id]
+        (shares || []).each do |share|
+          res << {
+            'user_id' => share['user_id'],
+            'record_code' => record_code,
+            'type' => 'board_share',
+            'old_school' => true,
+            'state' => {
+              'board_key' => record.key,
+              'sharer_id' => author.global_id,
+              'sharer_user_name' => author.user_name,
+              'include_downstream' => !!share['include_downstream'],
+              'allow_editing' => !!share['allow_editing'],
+              'pending' => !!share['pending'],
+              'user_name' => share['user_name']
+            }
+          }
+        end
+      end
+    end
     
     # expires = 72.hours.to_i
     # Permissable.permissions_redis.setex(cache_key, expires, res.to_json)
