e831d50bc316f3b00f18e7591d4d37a4df443d46
Brian Whitmer <brian.whitmer@gmail.com>
2019-05-14 16:35:28 -0600

    deployment compilation for 2019.05.14e

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -74,8 +74,8 @@ class UserMailer < ActionMailer::Base
   def log_message(user_id, log_id)
     @user = User.find_by_global_id(user_id)
     @log = LogSession.find_by_global_id(log_id)
-    @author_name = (@log.data['author_contact'] || {})['name'] || @author.settings['name']
     @author = @log.author
+    @author_name = (@log.data['author_contact'] || {})['name'] || @author.settings['name']
     @target = @log.user
     mail_message(@user, "New Message")
   end
