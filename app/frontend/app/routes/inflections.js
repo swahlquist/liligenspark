6250673269fed51275a41a30c93a52ad690da9f3
Brian Whitmer <brian.whitmer@gmail.com>
2019-05-30 12:02:07 -0600

    missing inflections route

---
+++ app/frontend/app/routes/inflections.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import Route from '@ember/routing/route';
+import app_state from '../utils/app_state';
+
+export default Route.extend({
+  title: "Inflections",
+  model: function(params) {
+    this.set('ref', params.ref);
+    this.set('locale', params.locale);
+    return {};
+  },
+  setupController: function(controller) {
+    controller.set('ref', this.get('ref'));
+    controller.set('locale', this.get('locale'));
+    controller.load_word();
+  }
+});
