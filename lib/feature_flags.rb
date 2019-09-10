848671f7b8eb8b9b716ab5cabf7a7bc8ef7cc8c3
Brian Whitmer <brian.whitmer@gmail.com>
2019-09-10 15:30:13 -0600

    enable new features

--- lib/feature_flags.rb
+++ lib/feature_flags.rb
@@ -11,7 +11,8 @@ module FeatureFlags
               'video_recording', 'goals', 'modeling', 'geo_sidebar', 'edit_before_copying',
               'core_reports', 'lessonpix', 'translation', 'fast_render',
               'audio_recordings', 'app_connections', 'enable_all_buttons', 'badge_progress',
-              'premium_symbols', 'board_levels', 'native_keyboard', 'app_store_purchases']
+              'premium_symbols', 'board_levels', 'native_keyboard', 'app_store_purchases',
+              'find_multiple_buttons', 'new_speak_menu']
   DISABLED_CANARY_FEATURES = []
   FEATURE_DATES = {
     'word_suggestion_images' => 'Jan 21, 2017',
