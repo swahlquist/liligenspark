20a2e7fe48cafc3df7b1a8cb8d3ffb564e07f8e0
Brian Whitmer <brian.whitmer@gmail.com>
2019-10-28 13:53:54 -0600

    option to restore paid account

--- app/frontend/app/controllers/user/index.js
+++ app/frontend/app/controllers/user/index.js
@@ -377,6 +377,8 @@ export default Controller.extend({
         this.set('subscription_settings', {action: action, type: i18n.t('add_premium_voice', "Add 1 Premium Voice")});
       } else if(action == 'enable_extras') {
         this.set('subscription_settings', {action: action, type: i18n.t('enable_extras', "Enable Premium Symbols Access")});
+      } else if(action == 'restore_purchase') {
+        this.set('subscription_settings', {action: action, type: i18n.t('restore_purchase', "Restore an Accidentally-Disabled Purchase")});
       } else if(action == 'force_logout') {
         this.set('subscription_settings', {action: action, type: i18n.t('force_device_logout', "Force Logout on all Devices (this may cause the user to lose some logs)")});
       }
