21a7921ebb0ac64463b871769afd85cfb7f0a0b3
Brian Whitmer <brian.whitmer@gmail.com>
2019-12-13 13:55:42 -0700

    obf bump

--- config/initializers/obf_footer.rb
+++ config/initializers/obf_footer.rb
@@ -1,3 +1,3 @@
 require 'obf'
-OBF::PDF.footer_text = "printed at mycoughdrop.com"
-OBF::PDF.footer_url = "https://www.mycoughdrop.com"
+OBF::PDF.footer_text = "printed at coughdrop.com"
+OBF::PDF.footer_url = "https://www.coughdrop.com"
