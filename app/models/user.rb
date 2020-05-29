9728fdbf59237200841924a808012e611fe55323
Brian Whitmer <brian.whitmer@gmail.com>
2020-05-29 15:40:14 -0600

    re-trial period people who lost their sponsorship

--- app/models/user.rb
+++ app/models/user.rb
@@ -332,7 +332,7 @@ class User < ActiveRecord::Base
       self.next_notification_at ||= next_notification_schedule
     end
     # Extend all trials until July 31, 2020
-    if (!self.expires_at && !self.id) || (self.grace_period?)
+    if (!self.expires_at && !self.id) || (self.grace_period?) || (!self.expires_at && !self.paid_or_sponsored?)
       self.expires_at = [self.expires_at || Date.today + 60, Date.parse('2020-07-31')].max
     end
     return false if self.user_name == ""
