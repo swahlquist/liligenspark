22a093295e04aa43450b7e1cef37138b8f600e91
Brian Whitmer <brian.whitmer@gmail.com>
2020-11-24 17:29:19 -0700

    enable remote modeling

--- lib/feature_flags.rb
+++ lib/feature_flags.rb
@@ -16,7 +16,7 @@ module FeatureFlags
               'premium_symbols', 'board_levels', 'native_keyboard', 'app_store_purchases',
               'find_multiple_buttons', 'new_speak_menu', 'swipe_pages', 'inflections_overlay',
               'ios_head_tracking', 'emergency_boards', 'evaluations',
-              'vertical_ios_head_tracking']
+              'vertical_ios_head_tracking', 'remote_modeling']
   DISABLED_CANARY_FEATURES = []
   FEATURE_DATES = {
     'word_suggestion_images' => 'Jan 21, 2017',
