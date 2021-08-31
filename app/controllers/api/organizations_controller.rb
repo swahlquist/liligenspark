59ac495e747be4a6a28efdbcf2c3bdd5df1c985f
Brian Whitmer <brian.whitmer@gmail.com>
2021-08-31 10:00:44 -0600

    typo

--- app/controllers/api/organizations_controller.rb
+++ app/controllers/api/organizations_controller.rb
@@ -377,6 +377,7 @@ class Api::OrganizationsController < ApplicationController
       params['organization'].delete('allotted_licenses') 
       params['organization'].delete('licenses_expire') 
       params['organization'].delete('include_extras')
+      params['organization'].delete('org_access')
     end
     if org.process(params['organization'], {'updater' => @api_user})
       render json: JsonApi::Organization.as_json(org, :wrapper => true, :permissions => @api_user).to_json
