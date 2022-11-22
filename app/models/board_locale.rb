5561ac3176ebfb35efcd2a31340c5b4df3f29f15
Brian Whitmer <brian.whitmer@gmail.com>
2022-11-22 12:19:36 -0700

    Trigger note

--- app/models/board_locale.rb
+++ app/models/board_locale.rb
@@ -1,4 +1,6 @@
 class BoardLocale < ApplicationRecord
+  # NOTE: There is a trigger defined within a migration for board_locales that
+  # does not show up in schema.rb
   belongs_to :board
   include PgSearch::Model
   pg_search_scope :search_by_text, :against => :search_string, :using => { :tsearch => {dictionary: 'simple', :tsvector_column => 'tsv_search_string'}} #, :ranked_by => "log(board_locales.popularity + board_locales.home_popularity + 3) * :tsearch"
